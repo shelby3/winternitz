60444d1d4f21755cf870af09f58f0ddf793c06a3
shelby3 <shelby3@users.noreply.github.com>
2015-12-06 15:59:25 +0800

    Update 1 subrepository shelby3/cmacros to revision b817c6cc5a0231a0ad9bb1e71a69a15df05e4d80

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Refer to /winternitz/[Winternitz.md] for more detailed documentation.
 
  Subrepository  | [Added] As |                Revision
 ----------------|------------|-----------------------------------------
-shelby3/cmacros | /cmacros   | bd4ea5bcb796c38e90f3f52b6552696667401e8d
+shelby3/cmacros | /cmacros   | b817c6cc5a0231a0ad9bb1e71a69a15df05e4d80
 
 [Added]: https://gist.github.com/shelby3/f69c969ecaa3ecfbe579#subrepositories
 [Winternitz.md]: https://github.com/shelby3/winternitz/blob/master/winternitz/Winternitz.md
--- cmacros/effectless.h
+++ cmacros/effectless.h
@@ -10,11 +10,11 @@ Generic min and max without double execution of side-effects:
   http://stackoverflow.com/questions/3437404/min-and-max-in-c
 */
 
-#define _CHOOSE(boolop, a, b, uid) \
-  ({                               \
-    decltype(a) _a_ ## uid = (a);  \
-    decltype(b) _b_ ## uid = (b);  \
-    (a) boolop (b) ? (a) : (b);    \
+#define _CHOOSE(boolop, a, b, uid)                                  \
+  ({                                                                \
+    decltype(a) _a_ ## uid = (a);                                   \
+    decltype(b) _b_ ## uid = (b);                                   \
+    (_a_ ## uid) boolop (_b_ ## uid) ? (_a_ ## uid) : (_b_ ## uid); \
   })
 #undef MIN
 #undef MAX
