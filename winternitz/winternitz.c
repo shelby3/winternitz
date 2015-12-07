eafb4d6a478275e6b5ab179bdc172f4dfdf65bec
shelby3 <shelby3@users.noreply.github.com>
2015-12-07 16:55:57 +0800

    Add 1 function: winternitz2; Add 1 typedef: winternitz_callback

--- winternitz/winternitz.c
+++ winternitz/winternitz.c
@@ -3,7 +3,7 @@
 #undef winternitz
 
 void* EXPORT
-winternitz(const bool sign, void* state, void* hash(void* state, uint32 n), const uint32* msg, size_t bits, const uint8 chunk_bits, const uint8* checksum_bits) {
+winternitz(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits, const uint8 chunk_bits, const uint8* checksum_bits) {
   if (chunk_bits > SIZEOF(*msg) * 8) return NULL;         // Error: message chunk size larger than *msg word size?   (would require more complex logic below to handle this case and winternitz is not practical for such large chunk sizes)
   if (bits % chunk_bits != 0) return NULL;                // Error: message is smaller than or not a multiple of chunk size? (or chunk_bits==0 && bits!=0)    (works even if bits<0 but bits is an unsigned type)
   uint8 t = 0;
@@ -51,7 +51,50 @@ winternitz(const bool sign, void* state, void* hash(void* state, uint32 n), cons
 }
 
 void* EXPORT
-merkle_lamport(const bool sign, void* state, void* hash(void* state, uint32 n), const uint32* msg, size_t bits/*1 bit:, const uint8 chunk_bits, const uint8* checksum_bits*/) {
+winternitz2(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits, const uint8 chunk_bits/*use chunk_bits:, const uint8* checksum_bits*/) {
+  if (chunk_bits > SIZEOF(*msg) * 8) return NULL;         // Error: message chunk size larger than *msg word size?   (would require more complex logic below to handle this case and winternitz is not practical for such large chunk sizes)
+  if (bits % chunk_bits != 0) return NULL;                // Error: message is smaller than or not a multiple of chunk size? (or chunk_bits==0 && bits!=0)    (works even if bits<0 but bits is an unsigned type)
+  uint32 chunk_max = ((uint32)1 << chunk_bits) - 1;
+  uint32 checksum_max = chunk_max * (bits / chunk_bits);
+  // Sign the chunks of the message
+  uint32 checksum = 0;
+  uint8 cur_bits = 0;
+  uint32 cur = 0;
+  while (bits != 0) {
+    uint32 chunk_val;
+    if (cur_bits - chunk_bits >= 0) {
+      chunk_val = cur & chunk_max;
+      cur_bits -= chunk_bits;
+      cur >>= chunk_bits;
+    } else if (cur_bits == 0) {
+      chunk_val = *msg & chunk_max;
+      cur_bits = SIZEOF(*msg) * 8 - chunk_bits;
+      cur = *msg >> chunk_bits;
+      msg += 1;
+    } else {
+      chunk_val = cur << (chunk_bits - cur_bits) | (*msg & chunk_max >> cur_bits);
+      cur_bits = SIZEOF(*msg) * 8 - (chunk_bits - cur_bits);
+      cur = *msg >> (chunk_bits - cur_bits);
+      msg += 1;
+    }
+    state = hash(state, sign ? chunk_max - chunk_val : chunk_val);
+    if (state == NULL) return NULL;                       // Error: hash function failed?
+    checksum += chunk_val;
+    bits -= chunk_bits;
+  }
+  // Sign the chunks of the checksum
+  while (checksum_max) {
+    if (chunk_max > checksum_max) chunk_max = checksum_max;
+    state = hash(state, sign ? chunk_max - (checksum & chunk_max) : checksum & chunk_max);
+    if (state == NULL) return NULL;                       // Error: hash function failed?
+    checksum >>= chunk_bits;
+    checksum_max >>= chunk_bits;
+  }
+  return state;
+}
+
+void* EXPORT
+merkle_lamport(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits/*1 bit:, const uint8 chunk_bits, const uint8* checksum_bits*/) {
   uint32 checksum_max = bits;
   // Sign the chunks of the message
   uint32 checksum = 0;
--- winternitz/winternitz.h
+++ winternitz/winternitz.h
@@ -24,20 +24,30 @@ k-th chunk signature.
 The 'checksum_bits' should be terminated with a zero value.
 */
 
-void* EXPORT
-winternitz(const bool sign, void* state, void* hash(void* state, uint32 n), const uint32* msg, size_t bits, const uint8 chunk_bits, const uint8* checksum_bits);
+typedef void* winternitz_callback(void* state, uint32 n);
 
 void* EXPORT
-merkle_lamport(const bool sign, void* state, void* hash(void* state, uint32 n), const uint32* msg, size_t bits/*1 bit:, const uint8 chunk_bits, const uint8* checksum_bits*/);
+winternitz(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits, const uint8 chunk_bits, const uint8* checksum_bits);
+
+void* EXPORT
+winternitz2(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits, const uint8 chunk_bits/*use chunk_bits:, const uint8* checksum_bits*/);
+
+void* EXPORT
+merkle_lamport(const bool sign, void* state, winternitz_callback hash, const uint32* msg, size_t bits/*1 bit:, const uint8 chunk_bits, const uint8* checksum_bits*/);
 
 #include "../cmacros/variadic.h"
 
-// winternitz() with 5 arguments is replaced with merkle_lamport() on those 5 arguments.
+/*
+winternitz() with 5 or 6 arguments is replaced with merkle_lamport() or
+winternitz2() on those 5 or 6 arguments respectively.
+*/
 
-#define   merkle_lamport_5(a, b, c, d, e) a, b, c, d, e
-#define   winternitz_7(a, b, c, d, e, f, g) a, b, c, d, e, f, g
-#define   winternitz_5_name() merkle_lamport
-#define   winternitz_7_name() winternitz
-#define   winternitz(...) NVARIADIC(winternitz, NUMARG7(__VA_ARGS__), __VA_ARGS__)
+#define merkle_lamport_5(a, b, c, d, e      ) a, b, c, d, e
+#define    winternitz2_6(a, b, c, d, e, f   ) a, b, c, d, e, f
+#define     winternitz_7(a, b, c, d, e, f, g) a, b, c, d, e, f, g
+#define winternitz_5_name() merkle_lamport
+#define winternitz_6_name() winternitz2
+#define winternitz_7_name() winternitz
+#define winternitz(...) NVARIADIC(winternitz, NUMARG7(__VA_ARGS__), __VA_ARGS__)
 
 #endif
